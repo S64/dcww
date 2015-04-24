20e9f5ccd020604fd5f1a2a1a93ff58e2b25c1fa
Shuma Yoshioka <s64.stdio@gmail.com>
2015-04-25 07:32:27 +0900

    Update

--- src/run_install_init.sh
+++ src/run_install_init.sh
@@ -3,13 +3,20 @@ cd /var/www/html
 
 wp plugin install error-log-monitor --activate
 
+wp plugin install theme-check --activate
 wp plugin install wp-multibyte-patch
-wp plugin install jetpack
-wp plugin install google-sitemap-generator
-wp plugin install pushpress
-wp plugin install wpmandrill
 wp plugin install akismet
+wp plugin install jetpack
+
+wp plugin install github-updater
 wp plugin install wordless
+
+wp theme install twentyten
+wp theme install twentyeleven
+wp theme install twentytwelve
+wp theme install twentythirteen
+wp theme install twentyfourteen
+wp theme install twentyfifteen
 
 user_init_script='/opt/wordpress/install_init.sh'
 
